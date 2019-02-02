e79af49fa12796440d35eac25f92982f75ee271d
Sankar Biswas <sankarb475@gmail.com>
2019-02-02 16:47:11 +0530

    Update IsPalindromeSolution.py

--- IsPalindromeSolution.py
+++ IsPalindromeSolution.py
@@ -1,3 +1,17 @@
+#Problem statement can be found here :: https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if(x<0): return False
+        if (int(str(abs(x))[::-1]) == x):
+            return True
+        return False
+
+
 class IsPalindromeSolution:
     def isPalindrome(self, x):
         if (x<0): return False
