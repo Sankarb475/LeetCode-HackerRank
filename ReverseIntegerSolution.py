38552704b0b398d6e1f9e1e038b32dc96ad21408
Sankar Biswas <sankarb475@gmail.com>
2019-02-03 22:01:12 +0530

    Create ReverseIntegerSolution.py

---
+++ ReverseIntegerSolution.py
@@ -0,0 +1,13 @@
+class ReverseIntegerSolution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        a = int(str(abs(x))[::-1])
+        if x>0 and a<(pow(2,31)-1):
+            return a
+        if x<0 and -a>pow(-2,31):
+            return -a
+        return 0
+    
