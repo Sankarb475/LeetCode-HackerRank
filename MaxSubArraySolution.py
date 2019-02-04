ad70fc69ab9fc8c4f33067fd0ebda1978dd83bac
Sankar Biswas <sankarb475@gmail.com>
2019-02-04 10:31:54 +0530

    Update MaxSubArraySolution.py

--- MaxSubArraySolution.py
+++ MaxSubArraySolution.py
@@ -1,5 +1,19 @@
 # solution of https://leetcode.com/problems/maximum-subarray/
 
+# O(n) solution 
+class MaxSubArraySolution:
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(1, len(nums)):
+            if nums[i-1] > 0:
+                nums[i] += nums[i-1]
+        return max(nums)
+
+
+# another way O(n^2) solution 
 class MaxSubArraySolution:
     def maxSubArray(self, nums):
         """
