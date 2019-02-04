3a1793b690d83af89cbc351e1dbaa821bd5aa045
Sankar Biswas <sankarb475@gmail.com>
2019-02-04 18:50:20 +0530

    Update SingleNumberSolution.py

--- SingleNumberSolution.py
+++ SingleNumberSolution.py
@@ -9,3 +9,24 @@ class SingleNumberSolution:
         b = list(set(nums))
         list1 = [i for i in nums if not i in b or b.remove(i)]
         return list(set(nums) - set(list1))[0]
+
+    
+    # another way to slove this
+    
+from collections import Counter
+
+class SingleNumberSolution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = dict(Counter(nums))
+        for keys, values in a.items():
+            if(values==1):
+                return keys
+        
+        
+                
+            
+        
