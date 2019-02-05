6aaa482a01a49c54013bdccccbd9d8629c1f5ea5
Sankar Biswas <sankarb475@gmail.com>
2019-02-05 23:28:47 +0530

    Update MaxAreaSolution.py

--- MaxAreaSolution.py
+++ MaxAreaSolution.py
@@ -10,3 +10,14 @@ class MaxAreaSolution:
             for j in range(i+1,len(a)):
                 b.append(abs((a[i][0] - a[j][0])*(min(a[i][1],a[j][1]))))
         return max(b)
+    
+    
+ # another way to solve this
+
+class MaxAreaSolution:
+    def maxArea(self, height: 'List[int]') -> 'int':
+        b = []
+        for i in range(len(height)-1):
+            for j in range(i+1,len(height)):
+                b.append(abs((j-i)*(min(height[i], height[j]))))
+        return max(b)
