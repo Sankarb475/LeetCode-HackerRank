0b07f8af8c19cf823262e390065d9fca43fc18d3
Sankar Biswas <sankarb475@gmail.com>
2019-02-07 22:49:19 +0530

    Create SumEvenAfterQueriesSolution.py

---
+++ SumEvenAfterQueriesSolution.py
@@ -0,0 +1,33 @@
+# solution of https://leetcode.com/problems/sum-of-even-numbers-after-queries
+
+# O(2n) solution
+
+class SumEvenAfterQueriesSolution:
+    def sumEvenAfterQueries(self, A: 'List[int]', queries: 'List[List[int]]') -> 'List[int]':
+        outList = []
+        sumOut = 0
+        for j in A:
+            if j % 2 == 0:
+                sumOut = sumOut + j
+        out = 0
+        for i in queries:
+            A[i[1]] = A[i[1]] + i[0]
+            if (A[i[1]] - i[0]) % 2 != 0:
+                if A[i[1]] % 2 == 0:
+                    sumOut = sumOut + A[i[1]]
+                else:
+                    sumOut = sumOut
+            else:
+                if A[i[1]] % 2 == 0:
+                    sumOut = sumOut + i[0]
+                else:
+                    sumOut = sumOut - (A[i[1]] - i[0])
+                    
+            outList.append(sumOut)
+        return outList
+            
+                
+            
+            
+            
+        
