3185e83ed6691fe5a435b03593897fc38b2c8eaf
Sankar Biswas <sankarb475@gmail.com>
2019-02-08 19:03:57 +0530

    Update PowerfulIntegersSolution.py

--- PowerfulIntegersSolution.py
+++ PowerfulIntegersSolution.py
@@ -1,5 +1,44 @@
 # solution of https://leetcode.com/problems/powerful-integers/
 
+
+#Very fast and memory efficient 
+
+class PowerfulIntegersSolution:
+    def powerfulIntegers(self, x: 'int', y: 'int', bound: 'int') -> 'List[int]':
+        sumVal = 0
+        i = -1
+        j = -1
+        list1 = []
+        sumOut = 0
+        if x==1 and y ==1:
+            if bound>=2:
+                return [2]
+            else:
+                return []
+        if x>1:
+            while sumOut <=bound:
+                i = i+1
+                sumOut = x ** i
+        sumOut = 0
+        if y >1:
+            while sumOut <= bound:
+                j = j+1
+                sumOut = y **j
+        else:
+            j = i
+            
+        if (x<=1):
+            i = j
+        for m in range(i):
+            for n in range(j):
+                sumOut = x**m + y ** n
+                if (sumOut <= bound):
+                    list1.append(sumOut)
+                 
+        return list(set(sorted(list1)))
+
+
+
 class PowerfulIntegersSolution:
     def powerfulIntegers(self, x: 'int', y: 'int', bound: 'int') -> 'List[int]':
         sumVal = 0
