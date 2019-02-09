2b5147499a20d6f0382992fcc3649a4ab4e7c52c
Sankar Biswas <sankarb475@gmail.com>
2019-02-09 09:21:50 +0530

    Update LargestPerimeterSolution.py

--- LargestPerimeterSolution.py
+++ LargestPerimeterSolution.py
@@ -7,4 +7,19 @@ class LargestPerimeterSolution:
             if A[i] < A[i + 1] + A[i + 2]:
                 return A[i] + A[i + 1] + A[i + 2]
         return 0
-        
+ 
+
+# Another solution, slow but accurate
+
+import itertools
+
+class LargestPerimeterSolution:
+    def largestPerimeter(A: 'List[int]') -> 'int':
+        b = list(itertools.combinations(A, 3))
+        list1 = [0]
+        for i in b:
+            c = sorted(i)
+            if c[0] + c[1] > c[2]:
+                list1.append(sum(c))
+
+        return max(list1)
