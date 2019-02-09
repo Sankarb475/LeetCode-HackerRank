9940e89bdc5e167599453fbd79129ae937b78aa1
Sankar Biswas <sankarb475@gmail.com>
2019-02-09 13:47:56 +0530

    Update PrimePalindromeSolution.scala

--- PrimePalindromeSolution.scala
+++ PrimePalindromeSolution.scala
@@ -2,6 +2,34 @@
 
 # slow and not accepted but accurate
 
+class Solution:
+    def primePalindrome(self, N):
+        m = True
+        if (N==1 or N ==2):
+            return 2
+        while m:
+            if int(str(N)[::-1]) == N:
+                if self.is_prime(N):
+                    return N
+            N = N + 1
+            
+    def is_prime(self,b):    
+        if (b<6):
+            for i in range(2,b):
+                if b%i == 0:
+                    return False
+            return True
+        for i in range (2,b//2+1):
+            if b%i == 0 :
+                return False
+        return True 
+
+
+
+
+
+# slow and not accepted but accurate
+
 class PrimePalindromeSolution:
     def primePalindrome(self, N):
         return 1
