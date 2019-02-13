721a308c401db547aedd746c3460850847189a3f
Sankar Biswas <sankarb475@gmail.com>
2019-02-13 17:52:15 +0530

    Update IntersectionSolution.py

--- IntersectionSolution.py
+++ IntersectionSolution.py
@@ -5,4 +5,21 @@
 class IntersectionSolution:
     def intersection(self, nums1: 'List[int]', nums2: 'List[int]') -> 'List[int]':
         return list(set(nums1).intersection(nums2))
+    
+  
+
+
+class IntersectionSolution:
+    def intersection(self, nums1: 'List[int]', nums2: 'List[int]') -> 'List[int]':
+        listOut = []
+        if len(nums1) > len(nums2):
+            for a in nums2:
+                if a in nums1:
+                    listOut.append(a)
+        else:
+            for a in nums1:
+                if a in nums2:
+                    listOut.append(a)
+            
+        return list(set(listOut))
         
