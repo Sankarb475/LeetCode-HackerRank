de0a00b60c4f46fdff0eb585511ce1382bb778d0
Sankar Biswas <sankarb475@gmail.com>
2019-02-14 22:59:32 +0530

    Create AddToArrayFormSolution.py

---
+++ AddToArrayFormSolution.py
@@ -0,0 +1,22 @@
+# solution of https://leetcode.com/problems/add-to-array-form-of-integer/
+
+#fast and memory efficient
+
+class AddToArrayFormSolution:
+    def addToArrayForm(self, A: 'List[int]', K: 'int') -> 'List[int]':
+        b = ''.join(list(map(str,A)))
+        c = list(str(int(b) + K))
+        return list(map(int,c))
+
+
+
+class AddToArrayFormSolution:
+    def addToArrayForm(self, A: 'List[int]', K: 'int') -> 'List[int]':
+	    ret = []
+
+	    while len(A) or K:
+		    if len(A): K += A.pop(-1)
+		    ret.append(K % 10)
+		    K //= 10
+
+	    return ret[::-1]
