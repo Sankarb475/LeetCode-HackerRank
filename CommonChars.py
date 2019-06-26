3cb33d01a7962dd0b200fd29151d6cc0fbd25aca
Sankar Biswas <sankarb475@gmail.com>
2019-06-26 12:03:49 +0530

    Update CommonChars.py

--- CommonChars.py
+++ CommonChars.py
@@ -24,3 +24,27 @@ class Solution:
                     index = 0
                     break
         return output
+    
+    
+# solution is based on finding count of each char for all the strings, and the minimum count would be the max number count
+# of occurence in all th elements
+
+# fast and small 
+
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        firstWord = set(list(A[0]))
+        output = []
+        for i in firstWord:
+            char_count = []
+            for j in A:
+                char_count.append(j.count(i))
+            output.extend([i] * min(char_count))
+        return output
+    
+    
+    
+    
+    
+                
+          
