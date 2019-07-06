cc00c31221a152403992b9b0452141a7b37f67ab
Sankar Biswas <sankarb475@gmail.com>
2019-07-05 20:01:00 -0700

    Update and rename heightChecker.py to HeightChecker.py

---
+++ HeightChecker.py
@@ -0,0 +1,12 @@
+# solution of https://leetcode.com/problems/height-checker/
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        heightsSorted = sorted(heights)
+        count = 0
+        for i in range(len(heights)):
+            if heights[i] != heightsSorted[i]:
+                count = count + 1
+        return count
+        
+        
