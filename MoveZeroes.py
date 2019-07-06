bbf7f08b106c65030c6df11e028c59d0b189a9bb
Sankar Biswas <sankarb475@gmail.com>
2019-07-05 21:19:31 -0700

    Create MoveZeroes.py

---
+++ MoveZeroes.py
@@ -0,0 +1,15 @@
+# solution of https://leetcode.com/problems/move-zeroes
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count = 0 # Shift count variable
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count += 1
+            else:
+                nums[i-count], nums[i] = nums[i], nums[i-count] # Swap the nubmer and zero
+
+        return
