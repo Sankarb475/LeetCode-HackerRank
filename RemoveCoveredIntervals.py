04ce1bf2a71ba483f9d677cea5c973909a6ea0c3
Sankar Biswas <sankarb475@gmail.com>
2019-12-19 23:48:28 +0530

    Update RemoveCoveredIntervals.py

--- RemoveCoveredIntervals.py
+++ RemoveCoveredIntervals.py
@@ -1,5 +1,22 @@
 # solution of https://leetcode.com/problems/remove-covered-intervals/
 
+
+# better solution : O(N)
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        if not intervals:
+            return 0
+        count = 1
+        currElem = intervals[0]
+        for i in intervals[1:]:
+            if currElem[1] < i[1]:
+                currElem = i
+                count = count + 1                
+        return count
+            
+
+# O(N*2)
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: x[0])
