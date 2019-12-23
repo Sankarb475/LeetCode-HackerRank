2aca91b1305250b9a79ad80d584cc4a66cfe0cc2
Sankar Biswas <sankarb475@gmail.com>
2019-12-23 18:03:50 +0530

    Update TreeNode.py

--- TreeNode.py
+++ TreeNode.py
@@ -21,3 +21,21 @@ class Solution:
                 dfs(root.right, depth+1)
         dfs(root,1)
         return self.maxDepth
+    
+    
+    
+    
+# one line solution 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1 if root else 0
+    
+    
