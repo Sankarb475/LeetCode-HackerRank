500dc8549f9ada16acc680e5d8b593dda43f9c88
Sankar Biswas <sankarb475@gmail.com>
2019-12-24 14:33:57 +0530

    Update SearchMatrix.py

--- SearchMatrix.py
+++ SearchMatrix.py
@@ -19,7 +19,29 @@ class Solution:
                 left = mid +1
             
                 
+# another binary search
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix:
+            return False
+        for i in matrix:
+            left = 0
+            right = len(i) -1
+            while left <= right:
+                mid = (left + right)//2
+                if i[mid] == target:
+                    return True
+                if i[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             
+                
+            
+            
+                
+    
             
                 
     
