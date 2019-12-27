9e2d30cd4e59263c35ade553c852153996a27ad6
Sankar Biswas <sankarb475@gmail.com>
2019-12-27 10:46:29 +0530

    Update IsPerfectSquare.py

--- IsPerfectSquare.py
+++ IsPerfectSquare.py
@@ -1,5 +1,23 @@
-#solution of https://leetcode.com/problems/valid-perfect-square/
+#solution of https://leetcode.com/problems/valid-perfect-square/ ==> Binary search
 
+# Using binary seacrh 
+from math import sqrt
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        end = num//2 + 1
+        start = 1
+        while end >= start:
+            mid = (end + start)//2
+            if mid * mid == num:
+                return True
+            elif mid*mid > num:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return False
+
+
+#using function
 from math import sqrt
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
