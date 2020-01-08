d4382db95e2d177eff36463d87efd761d653898f
Sankar Biswas <sankarb475@gmail.com>
2020-01-08 16:06:04 +0530

    Create BubbleSort.py

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,2 @@
+# Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+# This algorithm does an extra round of traversing just to know that the list is already sorted.
