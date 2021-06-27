e7f28834dcac887b4785b9296ab088072074088a
Sankar Biswas <sankarb475@gmail.com>
2021-06-27 11:02:32 +0530

    Update SearchInsertSolution.py

--- SearchInsertSolution.py
+++ SearchInsertSolution.py
@@ -1,5 +1,32 @@
 # solution of https://leetcode.com/problems/search-insert-position/
 
+# follows normal binary search 
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) -1
+        mid = 0 
+        while low<=high:
+            mid = (low+high)//2
+            
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] < target:
+                low = mid + 1
+                if low > high:
+                    return low
+                
+                
+            elif nums[mid] > target:
+                high = mid - 1
+                if low > high:
+                    return low
+                
+
+
+===================================================================================
+
 class SearchInsertSolution:
     def searchInsert(self, nums, target):
         """
